b92bc1a5829468da9e14ddbea0952364a58af4ea
Dominik Bilicki <dkbilicki@gmail.com>
2017-12-06 19:28:50 +0100

    Banner component cleanup

--- frontend/src/app/banner/banner.component.ts
+++ frontend/src/app/banner/banner.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'volontulo-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class BannerComponent { }
